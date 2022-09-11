78ac403b4bb6a5fce3c29b4d3c8b8bb765540583
Dleize <30454929+Dleize@users.noreply.github.com>
2022-09-11 19:46:16 -0300

    Criação do projeto Spring

---
+++ testedimdim/src/main/java/br/com/dimdim/testedimdim/TestedimdimApplication.java
@@ -0,0 +1,13 @@
+package br.com.dimdim.testedimdim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestedimdimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestedimdimApplication.class, args);
+	}
+
+}
